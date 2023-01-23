5d4cf2e0eb2dd80e7643f120c1cddf01476316a3
Archie <archieoi@gmail.com>
2023-01-23 11:00:04 -0800

    Inital commit, when pullign add folders that are being ingnored to keep it working

---
+++ Source/EOSTutorial/EOSTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EOSTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEOSTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEOSTutorialGameMode();
+};
+
+
+
