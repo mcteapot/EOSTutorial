e300fdf1da997ac75c6f6756130217321f42829b
amgiarchiep <archie.prakash@amgistudios.com>
2023-01-24 11:25:06 -0800

    sessions connecting via the menu and linked to new map

---
+++ Source/EOSTutorial/GameModes/EOSGameModeEOSTuotiral.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EOSGameModeEOSTuotiral.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOSTUTORIAL_API AEOSGameModeEOSTuotiral : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
